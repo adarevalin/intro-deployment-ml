0ada0c83a79bbee03d68fff5b5fb8a97b7dfd8b3
Andres David <kyfunky1386@gmail.com>
2024-06-17 18:51:07 -0500

    model

--- api/app/models.py
+++ api/app/models.py
@@ -7,9 +7,9 @@ class PredictionRequest(BaseModel):
     title_year: int
     aspect_ratio: float
     duration: int
-    cast_total_facebook_likes: float
     budget: float
     imdb_score: float
+
 
 class PredictionResponse(BaseModel):
     worldwide_gross: float
