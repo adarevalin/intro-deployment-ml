1d3ef5e6e22b24e8299c58ff44220af1407dde2c
Andres David <kyfunky1386@gmail.com>
2024-06-19 00:30:36 -0500

    Adding workflow testing 9

--- utilities/setter.py
+++ utilities/setter.py
@@ -3,12 +3,10 @@ from base64 import b64decode
 
 def main():
     key = os.environ.get('SERVICE_ACCOUNT_KEY')
-    if key:
-        with open('path.json', 'w') as json_file:
+    with open('path.json', 'w') as json_file:
             json_file.write(b64decode(key).decode())
-        print(os.path.realpath('path.json'))
-    else:
-        print("SERVICE_ACCOUNT_KEY is not set.")
+    print(os.path.realpath('path.json'))
+
 
 if __name__ == '__main__':
     main()
