8b5f4e6b36187183e0102d229e2838cef82e21cc
Andres David <kyfunky1386@gmail.com>
2024-06-21 22:40:03 -0500

    modelo

--- test.py
+++ test.py
@@ -3,21 +3,6 @@ from api.main import app
 
 client = TestClient(app)
 
-def test_null_prediction():
-    response = client.post('v1/prediction', json = {
-            'opening_gross': 0,
-            'screens': 0,
-            'production_budget': 0,
-            'title_year': 0,
-            'aspect_ratio': 0,
-            'duration': 0,
-            'budget': 0,
-            'imdb_score': 0
-    } )
-
-    assert response.status_code == 200
-    assert response.json()['worldwide_gross'] == 0
-
 def test_random_prediction():
     response = client.post('v1/prediction', json = {
                     'opening_gross': 8330681,
